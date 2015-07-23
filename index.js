4e69ee94d608e858162c6d04711c2db6da39cb0a
Alex Barry <mrozbarry@pairshaped.ca>
2015-07-23 08:56:32 -0400

    Fixed the production app to run on a port set by the environment.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     morgan = require('morgan');
 
+app.set('port', (process.env.PORT || 8080));
+
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,7 +13,7 @@ app.get('*', function(req, res) {
   res.sendfile('./public/index.html');
 });
 
-var server = app.listen(8080, function() {
+var server = app.listen(app.get('port'), '0.0.0.0', function() {
   console.log('Server running', server.address().address, server.address().port);
 })
 
