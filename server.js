8a85db066e4597c06b673f39ac64c4ebf8e23d93
Alex <alex.barry@gmail.com>
2016-10-29 21:19:04 -0400

    Removed prod bundles, added server.js and express.

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
