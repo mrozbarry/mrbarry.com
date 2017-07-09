fed16aba8fa14054a38bf17f191f8e3c80cc9a4c
Alex Barry <alex.barry@gmail.com>
2017-07-09 19:43:11 -0400

    Updated express, added start script.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var app = express();
-var path = require("path");
+var express = require("express")
+var app = express()
+var path = require("path")
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 8080))
 
-app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.static(path.resolve(__dirname, "public")))
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  console.log('Node app is running on port', app.get('port'))
+})
