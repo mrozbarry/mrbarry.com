5caa5c4604a4aae4e0ebaf8caf7bac3070ee319f
Alex Barry <alex.barry@gmail.com>
2020-08-11 12:15:35 -0400

    Note Hyperapp version for course

--- bin/data/courses.js
+++ bin/data/courses.js
@@ -1,5 +1,9 @@
 import make from './_item';
 
 export default [
-  make('/courses/hands-on-web-development-with-hyperapp.html', 'Hands on Web Development with Hyperapp'),
+  make(
+    '/courses/hands-on-web-development-with-hyperapp.html',
+    'Hands on Web Development with Hyperapp',
+    ['hyperapp v2.0.1'],
+  ),
 ];
