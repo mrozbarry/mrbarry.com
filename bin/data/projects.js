41ed0e21765c9c6ecf274bf1fe1443ff3192f7b5
Alex Barry <alex.barry@gmail.com>
2020-08-10 23:41:37 -0400

    Fix FerpJS link

--- bin/data/projects.js
+++ bin/data/projects.js
@@ -1,7 +1,7 @@
 import make from './_item';
 
 export default [
-  make('https://ferp.mrozbarry.com', 'FerpJS'),
+  make('https://ferp.mrbarry.com', 'FerpJS'),
   make('https://mobti.me/', 'Mobtime'),
   make('https://github.com/mrozbarry/declarativas', 'Declarativas'),
 ];
