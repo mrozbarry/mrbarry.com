99bd98e6c8bb35f434488ba79d9ece1f0e58f0fd
Alex Barry <alex.barry@gmail.com>
2022-12-03 14:20:42 -0500

    Fixed link to my dotfiles project

--- bin/data/projects.js
+++ bin/data/projects.js
@@ -4,7 +4,7 @@ export default [
   make('https://github.com/ferp-js/ferp', 'FerpJS', ['functional', 'framework', 'library', 'npm']),
   make('https://github.com/mrozbarry/declarativas', 'Declarativas', ['declarative', 'javascript', 'canvas2d', 'library']),
   make('https://github.com/mrozbarry/localhost-ready', 'localhost-ready', ['cli app', 'unix', 'networking']),
-  make('https://github.com/mrozbarry/dot-files', 'Dot files', ['cli', 'zsh', 'config']),
+  make('https://github.com/mrozbarry/dotfiles', 'Dot files', ['cli', 'zsh', 'config']),
   make('https://github.com/mrozbarry/smash', 'Smash', ['game', 'declarativas', 'hyperapp', 'smash bros clone', 'web app']),
   make('https://github.com/Elm-Canvas/canvas', 'Elm Canvas', ['functional', 'elm 0.17', 'canvas2d', 'deprecated']),
   make('https://mobti.me/', 'Mobtime', ['hyperapp', 'mob programming', 'web app']),
