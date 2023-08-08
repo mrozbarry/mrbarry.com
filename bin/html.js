c2e5c5f7a2818048a36bd1b8167073a94f78fc88
Alex Barry <alex.barry@gmail.com>
2023-08-08 11:17:15 -0400

    Updated build so I can use inline markdown links

--- bin/html.js
+++ bin/html.js
@@ -17,11 +17,20 @@ const publicPath = path.join(rootPath, 'public');
 const pages = [
   { src: 'index.pug', props: { skills, languages, tools, friends, courses, projects } },
   { src: 'testimony.pug', props: {} },
+  { src: 'the-gospel.pug', props: {} },
   { src: 'projects/index.pug', props: { projects } },
   { src: 'courses/index.pug', props: { courses } },
   { src: 'courses/hands-on-web-development-with-hyperapp.pug', props: {} },
   { src: 'test-bed/index.pug', props: {} },
 ];
+
+const convertMarkdownLinks = (source) => {
+  const markdownLinkFinder = /\[([^\]]+)\]\(([^\)]+)\)/gm;
+  return source.replaceAll(markdownLinkFinder, (match, text, url) => {
+    console.log('replace', match, text, url);
+    return `<a href="${url}" class="underline text-blue-500" target="_blank">${text}</a>`;
+  });
+}
 
 Promise.all(pages.map(({ src, props }) => {
   const dest = src.replace(/pug$/, 'html');
@@ -30,7 +39,8 @@ Promise.all(pages.map(({ src, props }) => {
   if (!existsSync(destPath)) {
     mkdirSync(destPath);
   }
-  return fs.writeFile(path.join(publicPath, dest), view(props), 'utf8');
+  console.log('writing', path.join(publicPath, dest));
+  return fs.writeFile(path.join(publicPath, dest), convertMarkdownLinks(view(props)), 'utf8');
 }))
   .then(() => console.log('pages built'))
   .catch((err) => console.error('unable to build pages', err));
